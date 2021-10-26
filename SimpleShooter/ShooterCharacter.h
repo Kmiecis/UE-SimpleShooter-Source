c2a5b8fb402e1515c1e01aa931d08c5540a0b82e
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2021-10-26 06:09:32 +0200

    Added character movement

---
+++ SimpleShooter/ShooterCharacter.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AShooterCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+};
